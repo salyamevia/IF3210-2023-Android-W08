abf94fdeb67250e8451c5408d93600bf8f9c1787
Rhapsodya Piedro Asmorobangun <piedrorhapsodya@gmail.com>
2023-02-23 22:04:21 +0700

    Resolve conflict

--- app/src/main/java/com/tubespbdandroid/majika/fragments/CartFragment.kt
+++ app/src/main/java/com/tubespbdandroid/majika/fragments/CartFragment.kt
@@ -7,7 +7,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import androidx.fragment.app.Fragment
 import com.tubespbdandroid.majika.PaymentActivity
 import com.tubespbdandroid.majika.R
 import kotlinx.android.synthetic.main.fragment_cart.*
