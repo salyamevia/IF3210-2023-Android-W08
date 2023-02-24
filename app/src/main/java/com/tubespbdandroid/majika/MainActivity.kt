740394b96ebb0a2f88096ed1b847896b4251062d
leocardhio <leocardhio@gmail.com>
2023-02-24 11:31:50 +0700

    feat: landscape orientation

--- app/src/main/java/com/tubespbdandroid/majika/MainActivity.kt
+++ app/src/main/java/com/tubespbdandroid/majika/MainActivity.kt
@@ -2,8 +2,10 @@ package com.tubespbdandroid.majika
 
 import android.app.SearchManager
 import android.content.Intent
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.system.Os.remove
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.fragment.app.commit
 import androidx.fragment.app.replace
@@ -14,56 +16,27 @@ import com.tubespbdandroid.majika.fragments.MenuFragment
 import com.tubespbdandroid.majika.fragments.SearchBarFragment
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var bottomNavigationView: BottomNavigationView
+    private var selectedBottomNavBarItemId = R.id.menu
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setLayoutViewBasedOnOrientation(resources.configuration.orientation)
         handleSearchIntent(intent)
 
 //        DELETE TO ENABLE NIGHT MODE
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-
-        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
-        supportFragmentManager.commit {
-            replace<MenuFragment>(R.id.container)
-        }
-
-        bottomNavigationView.setOnItemSelectedListener {
-            when (it.itemId) {
-                R.id.menu -> {
-                    supportFragmentManager.commit {
-                        setReorderingAllowed(true)
-                        replace<SearchBarFragment>(R.id.search_bar_container)
-                        replace<MenuFragment>(R.id.container)
-                        addToBackStack("menu")
-                    }
-                    true
-                }
-                R.id.cart -> {
-                    val searchBarFragment = supportFragmentManager.findFragmentById(R.id.search_bar_container) as SearchBarFragment?
-                    supportFragmentManager.commit{
-                        setReorderingAllowed(true)
-                        replace<CartFragment>(R.id.container)
-                        if (searchBarFragment != null) {
-                            remove(searchBarFragment)
-                        }
-                        addToBackStack("cart")
-                    }
-                    true
-                }
-                R.id.branch -> {
-                    supportFragmentManager.commit{
-                        replace<BranchFragment>(R.id.container)
-                    }
-                    true
-                }
-                else -> false
-            }
-        }
     }
 
     override fun onNewIntent(intent: Intent) {
         super.onNewIntent(intent)
         handleSearchIntent(intent)
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+
+        setLayoutViewBasedOnOrientation(newConfig.orientation)
     }
 
     private fun handleSearchIntent(intent: Intent) {
@@ -85,6 +58,59 @@ class MainActivity : AppCompatActivity() {
             println(error.message)
         }
 
+    }
+
+    private fun initBottomNavBarListener() {
+        bottomNavigationView.setOnItemSelectedListener {
+            selectedBottomNavBarItemId = it.itemId
+            val searchBarFragment = supportFragmentManager.findFragmentById(R.id.search_bar_container) as SearchBarFragment?
+            supportFragmentManager.commit{
+                if (searchBarFragment != null) {
+                    remove(searchBarFragment)
+                }
+            }
+            when (it.itemId) {
+                R.id.menu -> {
+                    supportFragmentManager.commit {
+                        setReorderingAllowed(true)
+                        replace<SearchBarFragment>(R.id.search_bar_container)
+                        replace<MenuFragment>(R.id.container)
+                        addToBackStack("menu")
+                    }
+                    true
+                }
+                R.id.cart -> {
+                    supportFragmentManager.commit{
+                        setReorderingAllowed(true)
+                        replace<CartFragment>(R.id.container)
+                        addToBackStack("cart")
+                    }
+                    true
+                }
+                R.id.branch -> {
+                    supportFragmentManager.commit{
+                        replace<BranchFragment>(R.id.container)
+                    }
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+
+    private fun setLayoutViewBasedOnOrientation(orientation: Int) {
+        when (orientation) {
+            Configuration.ORIENTATION_PORTRAIT -> {
+                setContentView(R.layout.activity_main)
+            }
+            Configuration.ORIENTATION_LANDSCAPE -> {
+                setContentView(R.layout.activity_main_landscape)
+            }
+        }
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation)
+        initBottomNavBarListener()
+        bottomNavigationView.selectedItemId = selectedBottomNavBarItemId
     }
 
 }
